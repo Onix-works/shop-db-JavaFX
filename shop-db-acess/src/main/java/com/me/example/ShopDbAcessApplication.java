e33fd1c7192923dc0285e8dd9bcab7624bdb6e58
Onix-works <61627180+Onix-works@users.noreply.github.com>
2020-11-05 22:30:46 +0300

    added jar

--- shop-db-acess/src/main/java/com/me/example/ShopDbAcessApplication.java
+++ shop-db-acess/src/main/java/com/me/example/ShopDbAcessApplication.java
@@ -1,7 +1,5 @@
 package com.me.example;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Scope;
